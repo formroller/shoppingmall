024c925cedb0ad43bd9781ea10a5d5d0f50ab54d
formroller <54848570+formroller@users.noreply.github.com>
2024-08-14 17:30:00 +0900

    OAuth2 Completed.

--- src/main/java/com/example/malls/domain/service/MemberService.java
+++ src/main/java/com/example/malls/domain/service/MemberService.java
@@ -1,2 +1,36 @@
-package com.example.malls.domain.service;public interface MemberService {
+package com.example.malls.domain.service;
+
+import com.example.malls.domain.dto.MemberJoinDTO;
+import com.example.malls.domain.entity.Member;
+
+public interface MemberService {
+    static class MidExistException extends Exception{}
+
+    void join(MemberJoinDTO memberJoinDTO) throws MidExistException;
+
+    /* 직렬화 역직렬화 */
+    default Member dtoToMember(MemberJoinDTO memberJoinDTO){
+        Member member = Member.builder()
+                .mid(memberJoinDTO.getMid())
+                .mpw(memberJoinDTO.getMpw())
+                .email(memberJoinDTO.getEmail())
+                .del(memberJoinDTO.isDel())
+                .social(memberJoinDTO.isSocial())
+                .build();
+
+        return member;
+    }
+
+    default MemberJoinDTO memberToDTO(Member member){
+        MemberJoinDTO joinDTO = MemberJoinDTO.builder()
+                .mid(member.getMid())
+                .mpw(member.getMpw())
+                .email(member.getEmail())
+                .del(member.isDel())
+                .social(member.isSocial())
+                .build();
+
+        return joinDTO;
+    }
+
 }
--- src/main/java/com/example/malls/domain/service/MemberServiceImpl.java
+++ src/main/java/com/example/malls/domain/service/MemberServiceImpl.java
@@ -1,2 +1,43 @@
-package com.example.malls.domain.service;public class MemberServiceImpl {
+package com.example.malls.domain.service;
+
+import com.example.malls.domain.dto.MemberJoinDTO;
+import com.example.malls.domain.entity.Member;
+import com.example.malls.domain.entity.MemberRole;
+import com.example.malls.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService{
+
+    private final MemberRepository memberRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public void join(MemberJoinDTO memberJoinDTO) throws MidExistException {
+        String mid = memberJoinDTO.getMid();
+        boolean exist = memberRepository.existsById(mid);
+
+        if(exist){
+            throw new MidExistException();
+        }
+
+        Member member = dtoToMember(memberJoinDTO);
+        member.changePassword(passwordEncoder.encode(memberJoinDTO.getMpw()));
+        member.addRole(MemberRole.USER);
+
+        log.info(" ======================= ");
+        log.info(member);
+        log.info(member.getRoleSet());
+        log.info(" ======================= ");
+
+        memberRepository.save(member);
+    }
 }
